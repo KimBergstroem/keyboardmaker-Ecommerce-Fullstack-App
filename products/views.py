b9e7c2d5e17eb71f901f324d82098872684e887b
Kim Bergstrom <kimmen_b@hotmail.com>
2023-11-14 16:46:04 +0000

    add: search functionality with queries #5 #22

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render,  get_object_or_404
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q # This is for searching queries
 from .models import Product
 
 
@@ -8,9 +10,29 @@ def all_products(request):
     and search queries
     """
     products = Product.objects.all()
+    query = None  # Initialize the 'query' variable to None
+
+    if request.GET:  # Check if there are any GET parameters in the request
+        if 'q' in request.GET:  # Check if the 'q' parameter exists in the GET parameters
+            query = request.GET['q']  # Retrieve the value of the 'q' parameter from the GET request
+
+            if not query:  # Check if the 'q' parameter is empty (no search criteria provided)
+                # If 'q' is empty, show an error message using the Django messages framework
+                messages.error(request, "You didn't enter any search criteria!")
+
+                # Redirect the user to a page named 'products' (likely a search results page)
+                return redirect(reverse('products'))
+
+    # Create a query for searching products based on name and description
+    # The | (OR) operator combines two conditions: name_icontains and description_icontains
+    queries = Q(name__icontains=query) | Q(description__icontains=query)
+
+    # Assuming 'products' is a queryset (e.g., a list of products), filter the products based on the queries
+    products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
