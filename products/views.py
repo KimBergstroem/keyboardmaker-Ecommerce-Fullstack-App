b5e5a9cf32b223a0f6e49e0f0422b4d28560ada1
Kim Bergstrom <kimmen_b@hotmail.com>
2023-11-14 20:01:33 +0000

    feat: sorting and product counts to product page #5

--- products/views.py
+++ products/views.py
@@ -30,6 +30,9 @@ def all_products(request):
             # Annotate the products queryset with a lowercase version of the 'name' field
             # This allows for case-insensitive sorting
             products = products.annotate(lower_name=Lower('name'))
+        
+        if sortkey == 'category':
+                sortkey = 'category__name'
 
         # Check if the sorting direction is specified
         if 'direction' in request.GET:
@@ -45,27 +48,26 @@ def all_products(request):
         products = products.order_by(sortkey)
 
     if 'category' in request.GET:
-                categories = request.GET['category'].split(',')
-                products = products.filter(category__name__in=categories)
-                categories = Category.objects.filter(name__in=categories)
+        categories = request.GET['category'].split(',')
+        products = products.filter(category__name__in=categories)
+        categories = Category.objects.filter(name__in=categories)
 
-    if request.GET:  # Check if there are any GET parameters in the request
-        if 'q' in request.GET:  # Check if the 'q' parameter exists in the GET parameters
-            query = request.GET['q']  # Retrieve the value of the 'q' parameter from the GET request
+    #Searchbar function in products
+    if 'q' in request.GET:  # Check if the 'q' parameter exists in the GET parameters
+        query = request.GET['q']  # Retrieve the value of the 'q' parameter from the GET request
 
-            if not query:  # Check if the 'q' parameter is empty (no search criteria provided)
-                # If 'q' is empty, show an error message using the Django messages framework
-                messages.error(request, "You didn't enter any search criteria!")
+        if not query:
+            messages.error(request, "You didn't enter any search criteria!")
 
-                # Redirect the user to a page named 'products' (likely a search results page)
-                return redirect(reverse('products'))
+            # Redirect the user to a page named 'products' (likely a search results page)
+            return redirect(reverse('products'))
 
-    # Create a query for searching products based on name and description
-    # The | (OR) operator combines two conditions: name_icontains and description_icontains
-    queries = Q(name__icontains=query) | Q(description__icontains=query)
+        # Create a query for searching products based on name and description
+        # The | (OR) operator combines two conditions: name_icontains and description_icontains
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
 
-    # Assuming 'products' is a queryset (e.g., a list of products), filter the products based on the queries
-    products = products.filter(queries)
+        # Assuming 'products' is a queryset (e.g., a list of products), filter the products based on the queries
+        products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
 
