6a4a2480744d295fc6077b8fab4b19957b0ed36c
Kim Bergstrom <kimmen_b@hotmail.com>
2023-11-14 19:29:10 +0000

    add: category filtering and href urls #5

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q # This is for searching queries
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
@@ -11,6 +11,12 @@ def all_products(request):
     """
     products = Product.objects.all()
     query = None  # Initialize the 'query' variable to None
+    categories = None
+
+    if 'category' in request.GET:
+                categories = request.GET['category'].split(',')
+                products = products.filter(category__name__in=categories)
+                categories = Category.objects.filter(name__in=categories)
 
     if request.GET:  # Check if there are any GET parameters in the request
         if 'q' in request.GET:  # Check if the 'q' parameter exists in the GET parameters
@@ -33,6 +39,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories
     }
 
     return render(request, 'products/products.html', context)
