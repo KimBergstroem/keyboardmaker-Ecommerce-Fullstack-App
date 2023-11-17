717bf44e2c1674dd250b2dd9d470af5b5a624198
Kim Bergstrom <kimmen_b@hotmail.com>
2023-11-17 13:54:15 +0000

    feat: add stripe elements #6 #27

--- checkout/views.py
+++ checkout/views.py
@@ -12,5 +12,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51O6uCHDYbVw9acKAvDoWn8klUVbltUrSdiPkajNwWp132LjTjyP7mcAowdagqKD2bOBYiPoi3vrz7zySq7vhYC2U00cRRM2gbz',
+        'client_secret': 'test client secret',
     }
     return render(request, template, context)
