778b29af2fce320a8377e04eaf6f47770a05a4b4
Kim Bergstrom <kimmen_b@hotmail.com>
2023-11-18 15:09:12 +0000

    feat: add_product view, url and template #7 #31

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.db.models.functions import Lower
 from django.contrib import messages
 from django.db.models import Q # This is for searching queries
 from .models import Product, Category
+from .forms import ProductForm
 
 
 def all_products(request):
@@ -92,3 +93,15 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ 
+    Add a product to the store 
+    """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
