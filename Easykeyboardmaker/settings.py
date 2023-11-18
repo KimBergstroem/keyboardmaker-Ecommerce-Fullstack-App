5e69c47d837b31af32c914e8a765d30cee53fabc
Kim Bergstrom <kimmen_b@hotmail.com>
2023-11-18 15:03:43 +0000

    feat: completed confirmation emails #30 #6

--- Easykeyboardmaker/settings.py
+++ Easykeyboardmaker/settings.py
@@ -209,6 +209,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 STRIPE_WH_SECRET = os.environ.get("STRIPE_WH_SECRET")
+DEFAULT_FROM_EMAIL = 'support@easykeyboard.com'
 
 # Email
 if 'DEVELOPMENT' in os.environ:
