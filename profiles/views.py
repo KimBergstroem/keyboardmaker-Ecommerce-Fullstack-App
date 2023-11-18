76a08be10557d8597cc71a18c3d5cd068eb4861f
Kim Bergstrom <kimmen_b@hotmail.com>
2023-11-18 14:39:14 +0000

    add: order history and updated checkout views #8 #34

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
 from .forms import UserProfileForm
 from django.contrib import messages
+from checkout.models import Order
 
 
 def profile(request):
@@ -23,5 +24,22 @@ def profile(request):
         'orders': orders,
         'on_profile_page': True # For making the success message only display the message and not the shopping bag
         # set this boolean to true and then in the code, AND NOT
+    }
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    """
+    Display users order history
+    """
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
     return render(request, template, context)
