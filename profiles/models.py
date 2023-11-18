8e2476f8b6e0c468593d316ba708265f74ff2d2c
Kim Bergstrom <kimmen_b@hotmail.com>
2023-11-18 14:12:08 +0000

    style: updated allauth templates and tested profile page #8 #34

--- profiles/models.py
+++ profiles/models.py
@@ -4,7 +4,6 @@ from django_countries.fields import CountryField
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-
 class UserProfile(models.Model):
     """
     A user profile model for maintaining default
@@ -12,16 +11,17 @@ class UserProfile(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Country *', null=True, blank=True) 
+    default_country = CountryField(blank_label='Country', null=True, blank=True) 
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
     
-    def __str__(sefl):
+    def __str__(self):
         return self.user.username
     
+
     @receiver(post_save, sender=User)
     def create_or_update_user_profile(sender, instance, created, **kwargs):
         """
@@ -30,4 +30,4 @@ class UserProfile(models.Model):
         if created:
             UserProfile.objects.create(user=instance)
         # Existing users: just save the profil
-        instance.userproifle.save()
+        instance.userprofile.save()
