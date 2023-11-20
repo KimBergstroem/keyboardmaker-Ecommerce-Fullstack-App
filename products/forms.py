29009570a131e96cf46e23ae105e40cc8ba4c7d1
Kim Bergstrom <kimmen_b@hotmail.com>
2023-11-20 12:43:06 +0000

    perf: resize image upload IF it's to big with pillow

--- products/forms.py
+++ products/forms.py
@@ -2,12 +2,10 @@ from django import forms
 from .models import Product, Category
 from .widgets import CustomClearableFileInput
 
-
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
-
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -16,3 +14,17 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
+
+    def save(self, commit=True):
+        # Call the save method of the parent class
+        instance = super().save(commit=False)
+
+        # Call the custom image_size_save method to handle image resizing
+        instance.image_size_save()
+
+        # Save the instance to the database
+        if commit:
+            instance.save()
+            self.save_m2m()
+
+        return instance
--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from PIL import Image
 
 class Category(models.Model):
     class Meta:
@@ -23,6 +24,23 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
+
+    def image_size_save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.image:
+            try:
+                img = Image.open(self.image.path)
+                max_size = (380, 380)
+
+                # Resize the image if it exceeds the maximum size
+                if img.height > max_size[1] or img.width > max_size[0]:
+                    img.thumbnail(max_size, Image.LANCZOS)
+                    img.save(self.image.path)
+            except Exception as e:
+                print(f"Error processing image for product {self.name}: {e}")
+
 
     def __str__(self):
         return self.name
