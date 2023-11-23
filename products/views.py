f0fb67564584575af6ef3166c5809a1b01d7618f
Kim Bergstrom <kimmen_b@hotmail.com>
2023-11-23 20:47:48 +0000

    add: IF reuqest.GET statement into the all_prodcuts def

--- products/views.py
+++ products/views.py
@@ -13,67 +13,71 @@ def all_products(request):
     and search queries
     """
     products = Product.objects.all()
-    query = None  # Initialize the 'query' variable to None
+    query = None  
     categories = None
     sort = None
     direction = None
 
-    # Check if sorting and filtering parameters are present in the GET request
-    if 'sort' in request.GET:
-        # Retrieve the sorting criteria from the 'sort' parameter
-        sortkey = request.GET['sort']
-        sort = sortkey  # (Optional) Assign the 'sort' variable
 
-        # Check if the sorting criteria is 'name'
-        if sortkey == 'name':
-            # Change the sorting key to 'lower_name' for case-insensitive sorting
-            sortkey = 'lower_name'
+    if request.GET: 
+        # Check if sorting and filtering parameters are present in the GET request
+        if 'sort' in request.GET:
+            # Retrieve the sorting criteria from the 'sort' parameter
+            sortkey = request.GET['sort']
+            sort = sortkey  # (Optional) Assign the 'sort' variable
 
-            # Annotate the products queryset with a lowercase version of the 'name' field
-            # This allows for case-insensitive sorting
-            products = products.annotate(lower_name=Lower('name'))
-        
-        if sortkey == 'category':
+            # Check if the sorting criteria is 'name'
+            if sortkey == 'name':
+                # Change the sorting key to 'lower_name' for case-insensitive sorting
+                sortkey = 'lower_name'
+
+                # Annotate the products queryset with a lowercase version of the 'name' field
+                # This allows for case-insensitive sorting
+                products = products.annotate(lower_name=Lower('name'))
+                
+            if sortkey == 'category':
                 sortkey = 'category__name'
 
-        # Check if the sorting direction is specified
-        if 'direction' in request.GET:
-            # Retrieve the sorting direction from the 'direction' parameter
-            direction = request.GET['direction']
+            # Check if the sorting direction is specified
+            if 'direction' in request.GET:
+                # Retrieve the sorting direction from the 'direction' parameter
+                direction = request.GET['direction']
 
-            # Check if the sorting direction is 'desc' (descending)
-            if direction == 'desc':
-                # Prepend '-' to the sorting key to indicate descending order
-                sortkey = f'-{sortkey}'
+                # Check if the sorting direction is 'desc' (descending)
+                if direction == 'desc':
+                    # Prepend '-' to the sorting key to indicate descending order
+                    sortkey = f'-{sortkey}'
 
-        # Sort the products queryset based on the chosen sorting criteria
-        products = products.order_by(sortkey)
+            # Sort the products queryset based on the chosen sorting criteria
+            products = products.order_by(sortkey)
 
-    if 'category' in request.GET:
-        categories = request.GET['category'].split(',')
-        products = products.filter(category__name__in=categories)
-        categories = Category.objects.filter(name__in=categories)
 
-    #Searchbar function in products
-    if 'q' in request.GET:  # Check if the 'q' parameter exists in the GET parameters
-        query = request.GET['q']  # Retrieve the value of the 'q' parameter from the GET request
+        # Search and display specific category in product
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
-        if not query:
-            messages.error(request, "You didn't enter any search criteria!")
+        #Searchbar function in products
+        if 'q' in request.GET:  # Check if the 'q' parameter exists in the GET parameters
+            query = request.GET['q']  # Retrieve the value of the 'q' parameter from the GET request
 
-            # Redirect the user to a page named 'products' (likely a search results page)
-            return redirect(reverse('products'))
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
 
-        # Create a query for searching products based on name and description
-        # The | (OR) operator combines two conditions: name_icontains and description_icontains
-        queries = Q(name__icontains=query) | Q(description__icontains=query)
+                # Redirect the user to a page named 'products' (likely a search results page)
+                return redirect(reverse('products'))
 
-        # Assuming 'products' is a queryset (e.g., a list of products), filter the products based on the queries
-        products = products.filter(queries)
+            # Create a query for searching products based on name and description
+            # The | (OR) operator combines two conditions: name_icontains and description_icontains
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+
+            # Assuming 'products' is a queryset (e.g., a list of products), filter the products based on the queries
+            products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
 
-    context = {
+    context = { # Most have context, for passing all the sorting, search function to render the page
         'products': products,
         'search_term': query,
         'current_categories': categories,
