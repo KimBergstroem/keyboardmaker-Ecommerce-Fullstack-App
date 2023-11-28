58249ea78fb4f7d30cdf16a12fae415ccc0cd0c8
Kim Bergstrom <kimmen_b@hotmail.com>
2023-11-28 16:22:49 +0000

    add: review view to admin dashboard

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
-
+from .models import Product, Category, Review
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
@@ -14,7 +13,6 @@ class ProductAdmin(admin.ModelAdmin):
 
     ordering = ('sku',)
 
-
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
@@ -23,3 +21,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Review)
