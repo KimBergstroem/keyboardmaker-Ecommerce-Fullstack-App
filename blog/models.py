9ef95f6abbbad3f64e1d72e0effe75b5896612cd
Kim Bergstrom <kimmen_b@hotmail.com>
2023-11-30 16:32:28 +0000

    style: add pagination to the article page #10 #42

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     """
     title = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
+    excerpt = models.TextField(max_length=75, blank=True)
     content = models.TextField(max_length=3000, blank=True)
     featured_image = models.ImageField(blank=True, upload_to='userprofile/', default="No img")
     author = models.ForeignKey(
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,63 @@
 from django.shortcuts import render
 from .models import Post
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
     """
-    View for displaying a list of blog posts on the homepage
+    View for displaying a list of blog posts on the homepage,
+    including sorting and search queries
     """
-    posts = Post.objects.filter(status=1).order_by("-created_on")
+    posts = Post.objects.filter(status=1)
+    query = None  
+    sort = None
+    direction = None
+    paginate_by = 4
+
+    if request.GET:
+        # Check and apply sorting parameters
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+
+            if sortkey == 'created_on':
+                if 'direction' in request.GET:
+                    direction = request.GET['direction']
+                    if direction == 'desc':
+                        sortkey = f'-{sortkey}'
+            else:
+                if 'direction' in request.GET:
+                    direction = request.GET['direction']
+                    if direction == 'desc':
+                        sortkey = f'-{sortkey}'
+                posts = posts.order_by(sortkey)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('blog'))
+
+            queries = Q(title__icontains=query) | Q(excerpt__icontains=query)
+            posts = posts.filter(queries)
+
+    # Create a string representation of the current sorting for display
+    current_sorting = f'{sort}_{direction}'
+
+    # Pagination 
+    paginator = Paginator(posts, paginate_by)
+    page = request.GET.get('page', 1)
     
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     context = {
         'posts': posts,
-        'paginate_by': 6,
+        'query': query,
+        'current_sorting': current_sorting,
+        'paginate_by': paginate_by,
     }
-
     return render(request, 'blog/blog.html', context)
