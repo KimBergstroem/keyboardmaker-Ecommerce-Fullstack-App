8f4f5c37832fbc945224f2139f42f9b849a6bafe
Kim Bergstrom <kimmen_b@hotmail.com>
2023-12-08 15:01:26 +0000

    fix: bug: Page not found (404) while product is removed but still in bag of users #59

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,22 +1,21 @@
 from django.conf import settings
 from decimal import Decimal
-from django.shortcuts import get_object_or_404
 from products.models import Product
 
 
 def bag_contents(request):
-    """
-    This is the context for calculating delivery is free or not
-    depending on customers order value
-    Function is cross all site
-    """
     bag_items = []
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-            product = get_object_or_404(Product, pk=item_id)
+    # Create a copy of the bag
+    bag_copy = bag.copy()
+
+    # Iterate over the original bag while modifying the copy
+    for item_id, quantity in bag_copy.items():
+        try:
+            product = Product.objects.get(pk=item_id)
             total += quantity * product.price
             product_count += quantity
             bag_items.append({
@@ -24,6 +23,11 @@ def bag_contents(request):
                 'quantity': quantity,
                 'product': product,
             })
+        except Product.DoesNotExist:
+            print(f"Product with ID {item_id} not found. Removing from the bag.")
+            # Remove the non-existent product from the bag
+            del bag[item_id]
+
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PRECENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
